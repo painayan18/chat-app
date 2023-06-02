7a33faf04c5ae70eedea5b7b1347278ba3362f77
phant <nayanspai@gmail.com>
2023-06-01 18:26:58 -0700

    index.js initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+
+  try {
+    const r =  await axios.put(
+    'https://api.chatengine.io/users/', 
+    { username: username, secret: username, first_name: username },
+    { headers: { "private-key": "5d75becc-f718-4c7f-a9f4-93ac61f5a555" } }
+    );
+
+    return res.status(r.status).json(r.data);
+
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+
 });
 
 app.listen(3001);
